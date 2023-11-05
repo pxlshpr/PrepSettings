3baf6110c2e6799207a1c2e1a86d5ea783f69fa1
Ahmed Khalaf <pxlshpr@gmail.com>
2023-11-05 19:35:18 +0500

    tag increment

--- Sources/PrepSettings/Extensions/Notification.swift
+++ Sources/PrepSettings/Extensions/Notification.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Notification.Name {
+public extension Notification.Name {
     static var didLoadCurrentBiometrics: Notification.Name { .init("didLoadCurrentBiometrics") }
     static var didSaveBiometrics: Notification.Name { .init("didSaveBiometrics") }
 }
