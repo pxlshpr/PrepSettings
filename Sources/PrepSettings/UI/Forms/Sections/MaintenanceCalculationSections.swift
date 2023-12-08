6d999ace46ebe2d070b02965ed23b41a54b12535
Ahmed Khalaf <pxlshpr@gmail.com>
2023-12-09 01:23:49 +0500

    tag increment

--- Sources/PrepSettings/UI/Forms/Sections/MaintenanceCalculationSections.swift
+++ Sources/PrepSettings/UI/Forms/Sections/MaintenanceCalculationSections.swift
@@ -63,7 +63,7 @@ struct MaintenanceAdaptiveSection: View {
 //        Text("Compares your weight change to the dietary energy you consumed to determine your adaptive maintenance. [Learn More.](https://example.com)")
 //        Text("Compares weight change to dietary energy consumed to determine your true maintenance, continuously adapting it to changes.")
 //        Text("The most accurate calculation of your maintenance energy.")
-        Text("The most accurate calculation based on your weight change and dietary energy consumption over a period.")
+        Text("An accurate calculation based on your weight change and dietary energy consumption over a period.")
     }
     
     var toggleRow: some View {
