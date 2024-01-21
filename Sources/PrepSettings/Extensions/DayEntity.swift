9fcc9764b395968749b7c0c0159b3a8b069f6dbd
Ahmed Khalaf <pxlshpr@gmail.com>
2024-01-22 00:54:00 +0500

    tag increment

---
+++ Sources/PrepSettings/Extensions/DayEntity.swift
@@ -0,0 +1,24 @@
+import Foundation
+import PrepShared
+
+public extension DayEntity {
+    var healthDetails: HealthDetails? {
+        get {
+            guard let healthData else { return nil }
+            do {
+                return try JSONDecoder().decode(HealthDetails.self, from: healthData)
+            } catch {
+                /// Error decoding, so set to nil
+                self.healthData = nil
+                return nil
+            }
+        }
+        set {
+            guard let newValue else {
+                self.healthData = nil
+                return
+            }
+            self.healthData = try! JSONEncoder().encode(newValue)
+        }
+    }
+}
