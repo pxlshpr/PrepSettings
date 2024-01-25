7fa05207e8678489c94dd5c26b1d25932ca2f886
Ahmed Khalaf <pxlshpr@gmail.com>
2024-01-25 23:44:03 +0500

    tag increment

--- Sources/PrepSettings/Observables/Provider/Provider+DisplayedDay.swift
+++ Sources/PrepSettings/Observables/Provider/Provider+DisplayedDay.swift
@@ -1,24 +1,26 @@
-import Foundation
-
-public extension Provider {
-    var displayedDate: Date {
-        get {
-            _displayedDate
-        }
-        set {
-            _displayedDate = newValue
-            
-            displayedDayChangeTask?.cancel()
-            displayedDayChangeTask = Task {
-                guard let fetchOrCreate = handlers?.day.fetchOrCreate else {
-                    fatalError()
-                }
-                let day = try await fetchOrCreate(newValue)
-                try Task.checkCancellation()
-                await MainActor.run {
-                    self.displayedDay = day
-                }
-            }
-        }
-    }
-}
+//import Foundation
+//
+//public extension Provider {
+//    var displayedDate: Date {
+//        get {
+//            _displayedDate
+//        }
+//        set {
+//            _displayedDate = newValue
+//            
+//            displayedDayChangeTask?.cancel()
+//            displayedDayChangeTask = Task {
+//                guard let fetchOrCreate = handlers?.day.fetchOrCreate else {
+//                    fatalError()
+//                }
+//                let day = try await fetchOrCreate(newValue)
+//                try Task.checkCancellation()
+//                await MainActor.run {
+//                    withAnimation(.snappy) {
+//                        self.displayedDay = day
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
